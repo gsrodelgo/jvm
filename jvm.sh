f93758c9d5b24e7a4d7b979980d574ad4118314b
gsrodelgo <61735565+gsrodelgo@users.noreply.github.com>
2020-03-03 14:28:35 +0100

    Update jvm.sh

--- jvm.sh
+++ jvm.sh
@@ -58,7 +58,7 @@ __jvm_pomversion() {
 	test ! -s "$pom" && return 1
 	tag="$(grep -Eo "$POM_REGEX" "$pom")"
 	test -z "$tag" && return 1
-	if [-z "${tag##*1.*}"]; then
+	if [-z "${tag##*.*}"]; then
 	  echo "$tag" |
 		cut -f2 -d'>' |
 		cut -f2 -d'.' |
